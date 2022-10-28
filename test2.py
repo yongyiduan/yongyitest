dabe825d1c42f90393bdfd6563da28b2eb36da20
yongyiduan <88175075+yongyiduan@users.noreply.github.com>
2022-10-28 20:24:18 +0800

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,15 @@
+import threading
+
+def print_test():
+    print('othre thread')
+    print('othre thread')
+    print('othre thread')
+
+def main():
+    print('this is main thread')
+    t = threading.Thread(target=print_test)
+    t.start()
+    t.join()
+
+if __name__ == '__main__':
+    main()
